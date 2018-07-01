0336ee8159a0f667577631a01db0d122133b4582
Roy Prigat <rp2719@columbia.edu>
2018-07-01 18:43:06 +0300

    added icons

--- background.js
+++ background.js
@@ -1,20 +1,34 @@
 
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onStartup.addListener(function () {
+
     // Set random background
-    (() => {
-        fetch('https://api.unsplash.com/photos/random?collections=2301297', {
-                headers: {
-                    Authorization: `Client-ID ${unsplashAPIKey}`
-                }
-            })
+    fetch(collectionUrl, {headers: {Authorization: `Client-ID ${unsplashAPIKey}`}})
+        .then((response) => response.json())
+        .then((data) => {
+            chrome.storage.sync.set({
+                wallpaper: `${data.urls.full}`
+            }, function () {});
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+    // Set location
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(({coords}) => {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${weatherAPIKey}&units=metric`)
             .then((response) => response.json())
             .then((data) => {
-                chrome.storage.sync.set({wallpaper: `${data.urls.full}`}, function () {})
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    })();
+                chrome.storage.sync.set({
+                    location: `${data.name}`,
+                    temprature: `${data.main.temp}`,
+                    weather: `${data.weather[0].main}`
+                }, function () {});
+            });
+        });
+    } else {
+        console.log('no geolocation');
+    }
 
     // chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     //     chrome.declarativeContent.onPageChanged.addRules([{
@@ -26,46 +40,4 @@ chrome.runtime.onInstalled.addListener(function () {
     //         actions: [new chrome.declarativeContent.ShowPageAction()]
     //     }]);
     // })
-
-
-    // https://source.unsplash.com/collection/1163637/1400x1000
-    // https://api.unsplash.com/photos/random?collections=1922729?width=1400
-    // { headers: {Authorization: `Client-ID ${unsplashAPIKey}`}}
-    // const updateBackground = (weather) => {
-    //     fetch('https://api.unsplash.com/photos/random?collections=1922729?width=1400?height=800', {
-    //             headers: {
-    //                 Authorization: `Client-ID ${unsplashAPIKey}`
-    //             }
-    //         })
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             document.body.style.backgroundImage = `url(${data.urls.full})`;
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //             // setDefaultBackground(weather);
-    //         })
-    // };
-
-    // const getLocalWeather = (latitude, longitude) => {
-    //     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherAPIKey}&units=imperial`)
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             mainText.innerText = `Weather in ${data.name} be like...`;
-    //             updateBackground(data.weather[0].main);
-    //         });
-    // };
-
-    // const getLocation = () => {
-    //     if ("geolocation" in navigator) {
-    //         navigator.geolocation.getCurrentPosition((position) => {
-    //             getLocalWeather(position.coords.latitude, position.coords.longitude);
-    //         });
-    //     } else {
-    //         console.log('no geolocation');
-    //     }
-    // };
-
-    // getLocation();
 });
--- main.js
+++ main.js
@@ -19,8 +19,20 @@
 //     });
 // };
 
+// Get wallpaper and set it to background
 chrome.storage.sync.get('wallpaper', ({wallpaper}) => {
     document.body.style.backgroundImage = `url('${wallpaper}')`;
-    document.getElementById('main-overlay').style.opacity = 0;
-    document.getElementById('main-overlay').style.zIndex = -1;
+    document.getElementById('container').style.opacity = 0;
+    document.getElementById('container').style.zIndex = -1;
 });
+
+// Get location and weather details
+chrome.storage.sync.get(['location', 'temprature', 'weather'], ({location, temprature, weather}) => {
+    document.getElementById('location').innerHTML = location;
+    document.getElementById('temp').innerHTML = Math.round(temprature) + "&#8451";
+    document.getElementById('w-icon').src = './img/sun.svg';
+    console.log(weather);
+});
+
+let date = new Date().toDateString();
+document.getElementById('date').innerHTML = `${date}`;
