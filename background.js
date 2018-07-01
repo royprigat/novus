9d71cd2dc7e6bd6c5b328da4c4f231be26875c25
Roy Prigat <rp2719@columbia.edu>
2018-07-01 12:13:18 +0300

    working random wallpaper

--- background.js
+++ background.js
@@ -1,18 +1,71 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({
-        color: '#3aa757'
-    }, function () {
-        console.log("The color is green.");
-    });
-    
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    })
-});
 
+chrome.runtime.onInstalled.addListener(function () {
+    // Set random background
+    (() => {
+        fetch('https://api.unsplash.com/photos/random?collections=2301297', {
+                headers: {
+                    Authorization: `Client-ID ${unsplashAPIKey}`
+                }
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                chrome.storage.sync.set({wallpaper: `${data.urls.full}`}, function () {})
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    })();
+
+    // chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    //     chrome.declarativeContent.onPageChanged.addRules([{
+    //         conditions: [new chrome.declarativeContent.PageStateMatcher({
+    //             pageUrl: {
+    //                 hostEquals: 'developer.chrome.com'
+    //             },
+    //         })],
+    //         actions: [new chrome.declarativeContent.ShowPageAction()]
+    //     }]);
+    // })
+
+
+    // https://source.unsplash.com/collection/1163637/1400x1000
+    // https://api.unsplash.com/photos/random?collections=1922729?width=1400
+    // { headers: {Authorization: `Client-ID ${unsplashAPIKey}`}}
+    // const updateBackground = (weather) => {
+    //     fetch('https://api.unsplash.com/photos/random?collections=1922729?width=1400?height=800', {
+    //             headers: {
+    //                 Authorization: `Client-ID ${unsplashAPIKey}`
+    //             }
+    //         })
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             document.body.style.backgroundImage = `url(${data.urls.full})`;
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //             // setDefaultBackground(weather);
+    //         })
+    // };
+
+    // const getLocalWeather = (latitude, longitude) => {
+    //     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherAPIKey}&units=imperial`)
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             mainText.innerText = `Weather in ${data.name} be like...`;
+    //             updateBackground(data.weather[0].main);
+    //         });
+    // };
+
+    // const getLocation = () => {
+    //     if ("geolocation" in navigator) {
+    //         navigator.geolocation.getCurrentPosition((position) => {
+    //             getLocalWeather(position.coords.latitude, position.coords.longitude);
+    //         });
+    //     } else {
+    //         console.log('no geolocation');
+    //     }
+    // };
+
+    // getLocation();
+});
--- main.js
+++ main.js
@@ -1,33 +1,26 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-    console.log(data.color);
+// chrome.storage.sync.get('color', function (data) {
+//     changeColor.style.backgroundColor = data.color;
+//     changeColor.setAttribute('value', data.color);
+//     console.log(data.color);
+// });
+
+// changeColor.onclick = function (element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({
+//         active: true,
+//         currentWindow: true
+//     }, function (tabs) {
+//         chrome.tabs.executeScript(
+//             tabs[0].id, {
+//                 code: 'document.body.style.backgroundColor = "' + color + '";'
+//             });
+//     });
+// };
+
+chrome.storage.sync.get('wallpaper', ({wallpaper}) => {
+    document.body.style.backgroundImage = `url('${wallpaper}')`;
+    document.getElementById('main-overlay').style.opacity = 0;
+    document.getElementById('main-overlay').style.zIndex = -1;
 });
-
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id, {
-                code: 'document.body.style.backgroundColor = "' + color + '";'
-            });
-    });
-};
-
-// const getLocation = () => {
-//     if ("geolocation" in navigator) {
-//         return navigator.geolocation.getCurrentPosition((position) => {
-//             console.log(position);
-//         });
-//     } else {
-//         console.log('no geolocation');
-//         return;
-//     }
-// }
-
-// getLocation();
